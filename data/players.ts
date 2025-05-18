103e2a4d4d1dada29e2feae67ac0963754c8660f
symensjeffrey <jeffrey.symens23@gmail.com>
2025-05-17 23:37:36 -0500

    update image path

--- data/players.ts
+++ data/players.ts
@@ -2,126 +2,126 @@ export const players = [
   {
     id: 1,
     name: 'Jeffrey Symens',
-    profileImage: '/images/profiles/jeffrey.jpg',
+    profileImage: 'images/profiles/jeffrey.jpg',
     wins: 3,
     statistics: "test"
   },
   {
     id: 2,
     name: 'Jacob Symens',
-    profileImage: '/images/profiles/jacob.jpg',
+    profileImage: 'images/profiles/jacob.jpg',
     wins: 2,
     statistics: "test"
   },
   {
     id: 3,
     name: 'Joshua Symens',
-    profileImage: '/images/profiles/joshua.jpg',
+    profileImage: 'images/profiles/joshua.jpg',
     wins: 2,
     statistics: "test"
   },
   {
     id: 4,
     name: 'Chloe Symens',
-    profileImage: '/images/profiles/chloe.jpg',
+    profileImage: 'images/profiles/chloe.jpg',
     wins: 0,
     statistics: "test"
   },
   {
     id: 5,
     name: 'Dean Symens',
-    profileImage: '/images/profiles/dean.jpg',
+    profileImage: 'images/profiles/dean.jpg',
     wins: 3,
     statistics: "test"
   },
   {
     id: 6,
     name: 'Steph Hill',
-    profileImage: '/images/profiles/steph.jpg',
+    profileImage: 'images/profiles/steph.jpg',
     wins: 4,
     statistics: "test"
   },
   {
     id: 7,
     name: 'Julie Jiracek',
-    profileImage: '/images/profiles/julie.jpg',
+    profileImage: 'images/profiles/julie.jpg',
     wins: 1,
     statistics: "test"
   },
   {
     id: 8,
     name: 'Jeff Hill',
-    profileImage: '/images/profiles/jeff.jpg',
+    profileImage: 'images/profiles/jeff.jpg',
     wins: 2,
     statistics: "test"
   },
   {
     id: 9,
     name: 'Ally Hill',
-    profileImage: '/images/profiles/ally.jpg',
+    profileImage: 'images/profiles/ally.jpg',
     wins: 3,
     statistics: "test"
   },
   {
     id: 10,
     name: 'Jenna Kelly',
-    profileImage: '/images/profiles/jenna.jpg',
+    profileImage: 'images/profiles/jenna.jpg',
     wins: 2,
     statistics: "test"
   },
   {
     id: 11,
     name: 'Josh Kelly',
-    profileImage: '/images/profiles/josh.jpg',
+    profileImage: 'images/profiles/josh.jpg',
     wins: 1,
     statistics: "test"
   },
   {
     id: 12,
     name: 'Erin Weston',
-    profileImage: '/images/profiles/erin.jpg',
+    profileImage: 'images/profiles/erin.jpg',
     wins: 0,
     statistics: "test"
   },
   {
     id: 13,
     name: 'Natalie',
-    profileImage: '/images/profiles/natalie.jpg',
+    profileImage: 'images/profiles/natalie.jpg',
     wins: 0,
     statistics: "test"
   },
   {
     id: 14,
     name: 'Princeton',
-    profileImage: '/images/profiles/princeton.jpg',
+    profileImage: 'images/profiles/princeton.jpg',
     wins: 0,
     statistics: "test"
   },
   {
     id: 15,
     name: 'Drew',
-    profileImage: '/images/profiles/drew.jpg',
+    profileImage: 'images/profiles/drew.jpg',
     wins: 1,
     statistics: "test"
   },
   {
     id: 17,
     name: 'Bryce Symens',
-    profileImage: '/images/profiles/bryce.jpg',
+    profileImage: 'images/profiles/bryce.jpg',
     wins: 4,
     statistics: "test"
   },
   {
     id: 18,
     name: 'Sue Hill',
-    profileImage: '/images/profiles/sue.jpg',
+    profileImage: 'images/profiles/sue.jpg',
     wins: 0,
     statistics: "test"
   },
   {
     id: 19,
     name: 'Craig Weston',
-    profileImage: '/images/profiles/craig.jpg',
+    profileImage: 'images/profiles/craig.jpg',
     wins: 0,
     statistics: "test"
   }
